49e1f6137b19094ab88e495ca8ed371bd262e36b
tanukhose <104750176+tanukhose@users.noreply.github.com>
2022-07-06 11:54:05 +0530

    Update function.Q.46.py

--- function.Q.46.py
+++ function.Q.46.py
@@ -17,3 +17,24 @@
 # 3 6 9 12 16
 # Sample Output:
 # False
+
+
+l=[1,2,3,5,7,9]
+i=0
+b=[]
+while i<len(l)-1:
+  k=l[i+1]-l[i]
+  b.append(k)
+  i+=1
+print(b)
+i=0
+c=0
+a=b[i]
+while i<len(b):
+  if b[i]==a:
+    c+=1
+  i+=1
+if c==len(b):
+  print("true")
+else:
+  print("false")
